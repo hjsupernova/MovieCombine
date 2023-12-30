6da6958c23a13d46370614fa1b7c2ea8eef0830e
hjsupernova <kem08120506@gmail.com>
2023-12-30 16:43:39 +0900

    Initial commit

---
+++ MovieCombine/MovieCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCombineApp.swift
+//  MovieCombine
+//
+//  Created by KHJ on 2023/12/30.
+//
+
+import SwiftUI
+
+@main
+struct MovieCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
